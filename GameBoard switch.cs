8de56f062e5645798de5a3ca3ab696315dc3f261
RNMancini <41347680+RNMancini@users.noreply.github.com>
2019-04-09 23:54:05 -0400

    clean code

--- GameBoard switch.cs
+++ GameBoard switch.cs
@@ -1,14 +1,16 @@
 Using System;
 public class Program
-{
-  public static void Main()
   {
-    //Console.WriteLine("Hello");
-    GameBoardticTacToeBoard = new GameBoard(3,3);
-    GameBoard connect4Board = new GameBoard(6, 7);
-    //GameBoard.drawBoard(3, 3);
-    ticTacToeBoard.drawboard();
-  }
+    
+    public static void Main()
+    {
+      //Console.WriteLine("Hello");
+      GameBoardticTacToeBoard = new GameBoard(3,3);
+      GameBoard connect4Board = new GameBoard(6, 7);
+      //GameBoard.drawBoard(3, 3);
+      ticTacToeBoard.drawboard();
+    }
+    
 }
 
 public class GameBoard(int height, int width)
